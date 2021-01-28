4d71feb0245ce0460e017b0dac3b403b814fd5c7
Jekyll <kalugin@outlook.com>
2021-01-28 13:54:26 +0300

    Add project files.

---
+++ parserTest/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace parserTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button_open = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button_dir = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(370, 752);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog_FileOk);
+            // 
+            // button_open
+            // 
+            this.button_open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_open.Location = new System.Drawing.Point(1025, 13);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(75, 23);
+            this.button_open.TabIndex = 1;
+            this.button_open.Text = "Open file";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.Button_open_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button_dir);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox);
+            this.splitContainer1.Size = new System.Drawing.Size(1112, 752);
+            this.splitContainer1.SplitterDistance = 370;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // button_dir
+            // 
+            this.button_dir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_dir.Location = new System.Drawing.Point(651, 42);
+            this.button_dir.Name = "button_dir";
+            this.button_dir.Size = new System.Drawing.Size(75, 23);
+            this.button_dir.TabIndex = 1;
+            this.button_dir.Text = "Check folder";
+            this.button_dir.UseVisualStyleBackColor = true;
+            this.button_dir.Click += new System.EventHandler(this.Button_dir_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.HideSelection = false;
+            this.textBox.Location = new System.Drawing.Point(0, 0);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox.Size = new System.Drawing.Size(738, 752);
+            this.textBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 752);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button_dir;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ parserTest/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace parserTest
+{
+    public partial class Form1 : Form
+    {
+        private List<JsonPathParser.JsonProperty> _pathList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+                return;
+
+            //var item = pathList.Where(n => n.Path == listBox1.SelectedItem.ToString());
+            var item = _pathList[listBox1.SelectedIndex];
+
+            var startPos = item.StartPosition;
+            var endPos = item.EndPosition;
+
+            if (startPos < 0 || startPos >= textBox.TextLength)
+                return;
+            if (endPos < 0 || endPos >= textBox.TextLength)
+                return;
+
+            textBox.SelectionStart = startPos;
+            textBox.SelectionLength = endPos - startPos + 1;
+            textBox.ScrollToCaret();
+            listBox1.Focus();
+        }
+
+        private void Button_open_Click(object sender, EventArgs e)
+        {
+            openFileDialog.FileName = "";
+            openFileDialog.Title = "Open json file";
+            openFileDialog.DefaultExt = "jsonc";
+            openFileDialog.Filter = "Kinetic files|*.jsonc|JSON files|*.json|All files|*.*";
+            openFileDialog.ShowDialog();
+        }
+
+        private void OpenFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            listBox1.Items.Clear();
+            //textBox.Text = JsonIO.BeautifyJson(File.ReadAllText(openFileDialog.FileName), true);
+            textBox.Text = File.ReadAllText(openFileDialog.FileName).Replace(' ', ' ');
+            Text = openFileDialog.FileName;
+            _pathList = new List<JsonPathParser.JsonProperty>();
+            _pathList = JsonPathParser.ParseJsonPathsStr(textBox.Text);
+            listBox1.Items.AddRange(_pathList.Select(n => n.Path).ToArray());
+        }
+
+        private void Button_dir_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK &&
+                !string.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+            {
+                var filesList = Directory.GetFiles(folderBrowserDialog1.SelectedPath,
+                    "*.jsonc",
+                    SearchOption.AllDirectories);
+
+                foreach (var file in filesList)
+                {
+                    var text = File.ReadAllText(file).Replace(' ', ' '); // replace &nbsp char with space
+                    var pathIndex = JsonPathParser.ParseJsonPathsStr(text, out var pos, out var error);
+
+                    // parsing failed
+                    if (error)
+                    {
+                        File.AppendAllText("_bad_file.txt",
+                              file + Environment.NewLine + pos + Environment.NewLine + Environment.NewLine);
+                    }
+                    // not failed but have incorrect positions
+                    var item1 = pathIndex.Where(n => n.EndPosition < 0 || n.StartPosition < 0);
+                    var jsonProperties = item1 as JsonPathParser.JsonProperty[] ?? item1.ToArray();
+                    if (jsonProperties.Any())
+                    {
+                        File.AppendAllText("_bad_file.txt", file + Environment.NewLine);
+                        foreach (var item in jsonProperties)
+                        {
+                            File.AppendAllText("_bad_file.txt",
+                                item.Path + " = " + item.Value + Environment.NewLine);
+                        }
+                        File.AppendAllText("_bad_file.txt", Environment.NewLine);
+                    }
+
+                    // find duplicate json paths
+                    var item2 = pathIndex.Where(n =>
+                    n.Type != JsonPathParser.PropertyType.Comment)
+                        .GroupBy(n => n.Path)
+                        .Where(n => n.Count() > 1);
+
+                    var enumerables = item2 as IGrouping<string, JsonPathParser.JsonProperty>[] ?? item2.ToArray();
+                    if (enumerables.Any())
+                    {
+                        File.AppendAllText("_dup_file.txt", file + Environment.NewLine);
+                        foreach (var item3 in enumerables)
+                        {
+                            foreach (var item4 in item3)
+                            {
+                                File.AppendAllText("_dup_file.txt", item4.Path
+                                  + " = " + item4.Value + Environment.NewLine);
+                            }
+                        }
+                        File.AppendAllText("_dup_file.txt", Environment.NewLine);
+                    }
+                }
+            }
+        }
+    }
+}
